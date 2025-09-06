7e24cc0af75b030b98d33611b2209f7015ad3b7d
Meek Msaki <meek10x@gmail.com>
2025-09-06 16:38:06 -0500

    update named abi

--- dev/generateAbi.sh
+++ dev/generateAbi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cast interface AsyncSwapCSMM --json | sed -e '1s/^/export const AsyncSwapCSMMAbi = /' -e '$a\'$'\n'' as const;' > ./packages/indexer/abis/AsyncSwapCSMM.ts
+cast interface AsyncSwap --json | sed -e '1s/^/export const AsyncSwapAbi = /' -e '$a\'$'\n'' as const;' > ./packages/indexer/abis/AsyncSwap.ts
 
 cast interface Router --json | sed -e '1s/^/export const RouterAbi = /' -e '$a\'$'\n'' as const;' > ./packages/indexer/abis/Router.ts
