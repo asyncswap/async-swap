e1518b3a1a71973333869c3cc1c6c51855284faa
Meek Msaki <meek10x@gmail.com>
2026-03-13 03:04:27 -0500

    check point 2

--- src/AsyncSwap.sol
+++ src/AsyncSwap.sol
@@ -7,20 +7,53 @@ import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
 import {ModifyLiquidityParams, SwapParams} from "v4-core/src/types/PoolOperation.sol";
 
 contract AsyncSwap layout at 1000 is IHooks {
     /// @notice The PoolManager contract address
     IPoolManager public immutable POOL_MANAGER;
 
+    /// @notice The minimum fee charged by the protocol in bips 1.2%
+    uint24 public minimumFee = 1_2000;
+
+    /// @notice The Owner
+    address public owner;
+
+    /// @notice Swap orders for input token0
+    /// @dev balancesIn is initialized by swapper and mutated by filler
+    mapping(address swapper => mapping(bool zeroForOne => uint256 amountGiven)) balancesIn;
+    /// @notice Swap orders for output token1
+    /// @dev balancesOut is initialized by swapper and mutated by filler
+    mapping(address swapper => mapping(bool zeroForOne => uint256 amountLeft)) balancesOut;
+
+    /// @param owner creator of order
+    /// @param deadline order expiration time
+    /// @param zeroForOne direction of order
+    /// @param sqrtPrice price to execute order
+    /// @param amount amount of token
+    struct Order {
+        address sender;
+        uint64 deadline;
+        bool zeroForOne;
+        uint160 sqrtPrice;
+        uint256 amountIn;
+        uint256 amountOut;
+    }
+
+    /// @notice A swap event
+    event Swap(bytes32 orderId, Order order);
+
     /// @notice Error if caller is not poolmanager address
     error CALLER_NOT_POOL_MANAGER();
+
     /// @notice Error for method not implemented
     error HOOK_NOT_IN_USE();
 
     /// @notice Initialize PoolManager storage variable
     constructor(IPoolManager _pm) {
         POOL_MANAGER = _pm;
+        owner = msg.sender;
     }
 
     /// @notice Only PoolManager contract allowed as msg.sender
@@ -67,21 +100,26 @@ contract AsyncSwap layout at 1000 is IHooks {
 
     function beforeInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96)
         external
-        pure
+        view
+        onlyPoolManager
         returns (bytes4)
     {
-        sender;
-        key;
+        require(sender == owner, "NOT HOOK OWNER");
+        require(address(key.hooks) == address(this));
+        require(key.fee >= 1_2000, "FEE SET TOO LOW"); // 1.2 %
         sqrtPriceX96;
         return this.beforeInitialize.selector;
     }
 
+    function getLatestPrice(Currency currency) public returns (uint256) {}
+
     function afterInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96, int24 tick)
         external
-        pure
+        view
+        onlyPoolManager
         returns (bytes4)
     {
-        sender;
+        require(sender == owner);
         key;
         sqrtPriceX96;
         tick;
@@ -90,6 +128,7 @@ contract AsyncSwap layout at 1000 is IHooks {
 
     function beforeSwap(address sender, PoolKey calldata key, SwapParams calldata params, bytes calldata hookData)
         external
+        onlyPoolManager
         returns (bytes4, BeforeSwapDelta, uint24)
     {}
 
