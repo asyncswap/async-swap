730b2d6829f0e8990910024c6071627d849a1f09
Meek Msaki <meek10x@gmail.com>
2026-03-13 01:31:07 -0500

    check point

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.34;
 
 import {Script} from "forge-std/Script.sol";
 import {AsyncSwap} from "../src/AsyncSwap.sol";
+import { PoolManager } from "v4-core/src/PoolManager.sol";
 
 contract CounterScript is Script {
     AsyncSwap public counter;
@@ -12,7 +13,8 @@ contract CounterScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        counter = new AsyncSwap();
+        PoolManager pm = new PoolManager(msg.sender);
+        counter = new AsyncSwap(pm);
 
         vm.stopBroadcast();
     }
--- src/AsyncSwap.sol
+++ src/AsyncSwap.sol
@@ -1,15 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.34;
 
-import { IPoolManager } from "v4-core/src/interfaces/IPoolManager.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BeforeSwapDelta} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {ModifyLiquidityParams, SwapParams} from "v4-core/src/types/PoolOperation.sol";
 
-contract AsyncSwap layout at 1000 {
-
+contract AsyncSwap layout at 1000 is IHooks {
     /// @notice The PoolManager contract address
-    IPoolManager public poolManager;
+    IPoolManager public immutable poolManager;
 
-    /// @notice Error if caller is not poolmanager address 
-    error CallerNotPoolManager();
+    /// @notice Error if caller is not poolmanager address
+    error CALLER_NOT_POOL_MANAGER();
+    /// @notice Error for method not implemented
+    error HOOK_NOT_IN_USE();
 
     /// @notice Initialize PoolManager storage variable
     constructor(IPoolManager _pm) {
@@ -18,7 +25,129 @@ contract AsyncSwap layout at 1000 {
 
     /// @notice Only PoolManager contract allowed as msg.sender
     modifier onlyPoolManager() {
-        require(msg.sender == address(poolManager), CallerNotPoolManager());
+        _onlyPoolManager();
         _;
+    }
+
+    /// @notice internal function requires caller is PoolManager
+    function _onlyPoolManager() internal view {
+        require(msg.sender == address(poolManager), CALLER_NOT_POOL_MANAGER());
+    }
+
+    /// @notice Validates the deployed hook
+    /// @param _this A Hook contract
+    function validateHookAddress(IHooks _this) internal pure {
+        Hooks.validateHookPermissions(_this, getHookPermissions());
+    }
+
+    /// @notice Returns activated hook permissions
+    /// @return permissions active hook perssions
+    function getHookPermissions() public pure returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: true, // true
+            afterInitialize: true, // true
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true, // true
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: true, // true
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    //////////////////////////
+    ///// HOOK ACTIVATED /////
+    //////////////////////////
+
+    function beforeInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96)
+        external
+        pure
+        returns (bytes4)
+    {
+        sender;
+        key;
+        sqrtPriceX96;
+        return this.beforeInitialize.selector;
+    }
+
+    function afterInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96, int24 tick)
+        external
+        pure
+        returns (bytes4)
+    {
+        sender;
+        key;
+        sqrtPriceX96;
+        tick;
+        return this.afterInitialize.selector;
+    }
+
+    function beforeSwap(address sender, PoolKey calldata key, SwapParams calldata params, bytes calldata hookData)
+        external
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {}
+
+    ///////////////////////////
+    ///// HOOK NOT IN USE /////
+    ///////////////////////////
+
+    function beforeAddLiquidity(address, PoolKey calldata, ModifyLiquidityParams calldata, bytes calldata)
+        external
+        pure
+        returns (bytes4)
+    {
+        revert HOOK_NOT_IN_USE();
+    }
+
+    function afterAddLiquidity(
+        address,
+        PoolKey calldata,
+        ModifyLiquidityParams calldata,
+        BalanceDelta,
+        BalanceDelta,
+        bytes calldata
+    ) external pure returns (bytes4, BalanceDelta) {
+        revert HOOK_NOT_IN_USE();
+    }
+
+    function beforeRemoveLiquidity(address, PoolKey calldata, ModifyLiquidityParams calldata, bytes calldata)
+        external
+        pure
+        returns (bytes4)
+    {
+        revert HOOK_NOT_IN_USE();
+    }
+
+    function afterRemoveLiquidity(
+        address,
+        PoolKey calldata,
+        ModifyLiquidityParams calldata,
+        BalanceDelta,
+        BalanceDelta,
+        bytes calldata
+    ) external pure returns (bytes4, BalanceDelta) {
+        revert HOOK_NOT_IN_USE();
+    }
+
+    function afterSwap(address, PoolKey calldata, SwapParams calldata, BalanceDelta, bytes calldata)
+        external
+        pure
+        returns (bytes4, int128)
+    {
+        revert HOOK_NOT_IN_USE();
+    }
+
+    function beforeDonate(address, PoolKey calldata, uint256, uint256, bytes calldata) external pure returns (bytes4) {
+        revert HOOK_NOT_IN_USE();
+    }
+
+    function afterDonate(address, PoolKey calldata, uint256, uint256, bytes calldata) external pure returns (bytes4) {
+        revert HOOK_NOT_IN_USE();
     }
 }
